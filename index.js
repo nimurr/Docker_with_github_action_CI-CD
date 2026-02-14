c8889c902905a846d288ee9ec4d0be007239bad2
nimurr <nimurnerob404@gmail.com>
2026-02-14 09:47:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Example app listening on port ${port}`)
+})
