1f812ae4d1b602082f75d685a63b5e36d01e73e3
nimurr <nimurnerob404@gmail.com>
2026-02-14 16:00:26 +0600

    `Update server IP in index.js`

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import app from "./server.js";
 
 const port = 3000;
 
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Server running at http://<YOUR_SERVER_IP>:${port}`);
+// Bind to your cloud IP
+app.listen(port, "172.31.21.163", () => {
+  console.log(`Server running at http://172.31.21.163:${port}`);
 });
