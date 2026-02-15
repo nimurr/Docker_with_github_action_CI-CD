c8fdc55d1727e10ab175c40939f4daf3401a3ca0
nimurr <nimurnerob404@gmail.com>
2026-02-15 09:17:28 +0600

    `Refactored server.js to add logging middleware and updated health endpoint response`

---
+++ src/utils/logers.js
@@ -0,0 +1,10 @@
+// loger middlewire 
+
+const logger = (req, res, next) => {
+    const time = new Date().toLocaleString();
+
+    console.log(`${req.method}- ${req.url}---- Date:${time}`);
+    next();
+}
+
+export default logger
