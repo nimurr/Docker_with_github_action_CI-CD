fe6b6a9e44a73eabe8381d4171866fb78f6cad88
nimurr <nimurnerob404@gmail.com>
2026-03-12 09:33:39 +0600

    `Update package-lock.json and server.js to reflect changes in dependencies and CORS configuration`

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.disable("x-powered-by");
 
 // CORS configuration
 app.use(cors({
-  origin: ["http://localhost:3001", "http://127.0.0.1:3001"],
+  origin: ["http://localhost:3001", "http://localhost:3000"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
